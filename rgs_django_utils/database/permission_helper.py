df2ab256bbee5de588f06fedc4b5328920dd9802
JeroenDuel2 <j.duel@tijhuisingenieurs.nl>
2026-04-30 12:12:54 +0200

    fix profile_measurments

--- rgs_django_utils/database/permission_helper.py
+++ rgs_django_utils/database/permission_helper.py
@@ -250,19 +250,18 @@ class PermissionHelper:
                             out_fr["select"] = True
                         if not out_fr["update"] and role_field_permission[2] == "u":
                             out_fr["update"] = True
-                    if presets is not None:
-                        role_presets = presets[role]  # falls back to "public" via __getitem__
-                        if len(role_presets) > 1:  # skip empty sentinel ("--",)
-                            if "preset_insert" not in out_fr:
-                                if role_presets[0][0] == "i":
-                                    out_fr["preset_insert"] = (True, role_presets[1])
-                                elif type(role_presets[0]) is tuple and role_presets[0][0][0] == "i":
-                                    out_fr["preset_insert"] = (True, role_presets[0][1])
-                            if "preset_update" not in out_fr:
-                                if role_presets[0][1] == "u":
-                                    out_fr["preset_update"] = (True, role_presets[1])
-                                elif type(role_presets[0]) is tuple and role_presets[1][0][1] == "u":
-                                    out_fr["preset_update"] = (True, role_presets[1][1])
+                    if presets is not None and role in presets.config:
+                        role_presets = presets.config[role]
+                        if "preset_insert" not in out_fr:
+                            if role_presets[0][0] == "i":
+                                out_fr["preset_insert"] = (True, role_presets[1])
+                            elif type(role_presets[0]) is tuple and role_presets[0][0][0] == "i":
+                                out_fr["preset_insert"] = (True, role_presets[0][1])
+                        if "preset_update" not in out_fr:
+                            if role_presets[0][1] == "u":
+                                out_fr["preset_update"] = (True, role_presets[1])
+                            elif type(role_presets[0]) is tuple and role_presets[1][0][1] == "u":
+                                out_fr["preset_update"] = (True, role_presets[1][1])
 
                 out[name][k] = out_fr
 
