bcaef5d3054a5a396c9304ff5dc423a1b66d3ed1
Bastiaan Roos <bastiaan@roosgeo.nl>
2026-05-20 11:15:20 +0200

    fix getting env settings from settings

--- rgs_django_utils/management/commands/generate_hasura_metadata.py
+++ rgs_django_utils/management/commands/generate_hasura_metadata.py
@@ -94,13 +94,12 @@ class Command(BaseCommand):
     def _send_metadata_to_hasura(self, metadata):
         from django.conf import settings
 
-        s = getattr(settings, "SettingsGetter", None)
-        if s is None:
+        if settings is None:
             hasura_url = os.environ.get("HASURA_GRAPHQL_URL")
             admin_secret = os.environ.get("HASURA_GRAPHQL_ADMIN_SECRET")
         else:
-            hasura_url = s.get("HASURA_GRAPHQL_URL")
-            admin_secret = s.get("HASURA_GRAPHQL_ADMIN_SECRET")
+            hasura_url = getattr(settings, "HASURA_GRAPHQL_URL")
+            admin_secret = getattr(settings, "HASURA_GRAPHQL_ADMIN_SECRET")
 
         if not hasura_url:
             self.stderr.write(
