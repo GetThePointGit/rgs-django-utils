bcd292a2067f22b9e8575208e9b2dfe57ce39c4a
JeroenDuel2 <j.duel@tijhuisingenieurs.nl>
2026-06-04 15:24:34 +0200

    fix project_edit

--- rgs_django_utils/database/base_models/validity_period.py
+++ rgs_django_utils/database/base_models/validity_period.py
@@ -10,8 +10,9 @@ class ValidityPeriodMixin(models.Model):
     or application-level guard on ``start_date <= end_date`` where needed.
     """
 
-    start_date = models.DateField(null=True, blank=True, config=models.Config(permissions=models.FPerm(org_mem="-s-", project_read="-s-")))
-    end_date = models.DateField(null=True, blank=True, config=models.Config(permissions=models.FPerm(org_mem="-s-", project_read="-s-")))
+    start_date = models.DateField(null=True, blank=True, config=models.Config(permissions=models.FPerm(org_mem="-s-", project_read="-s-", project_edit="isu")))
+
+    end_date = models.DateField(null=True, blank=True, config=models.Config(permissions=models.FPerm(org_mem="-s-", project_read="-s-", project_edit="isu")))
 
     class Meta:
         abstract = True
